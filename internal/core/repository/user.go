04a22687cb7fd5d2110ada4518037663c8bdea52
prinnnnnn <106861903+prinnnnnn@users.noreply.github.com>
2024-10-02 09:45:07 +0700

    fixed handle not found error

--- internal/core/repository/user.go
+++ internal/core/repository/user.go
@@ -13,6 +13,7 @@ import (
 	"fiber-server-1/internal/core/models"
 	"fmt"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -37,7 +38,6 @@ func (ur *UserRepository) GetUserById(ctx context.Context, id uint) (*models.Use
 	var user models.User
 	query := `SELECT * FROM users WHERE id = $1`
 
-	// Execute the query with named arguments to fetch the user details from the database.
 	err := ur.db.QueryRow(ctx, query, id).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt, &user.FirstName,
 		&user.LastName, &user.Email, &user.Password, &user.Location,
 		&user.Occupation, &user.ViewedProfile, &user.Impressions)
@@ -45,7 +45,7 @@ func (ur *UserRepository) GetUserById(ctx context.Context, id uint) (*models.Use
 	if err != nil {
 
 		// handle not found
-		if err.Error() == "no rows in result set" {
+		if err == pgx.ErrNoRows {
 			return nil, errors.New("user not found")
 		}
 
@@ -60,7 +60,7 @@ func (ur *UserRepository) GetUserById(ctx context.Context, id uint) (*models.Use
 func (ur *UserRepository) GetUserFriends(ctx context.Context, id uint) ([]models.UserResponse, error) {
 	var friendships []models.Friendship
 
-	// SQL Query: Select friendships where the user is either in user_id1 or user_id2
+	// Select friendships where the user is either in user_id1 or user_id2
 	query := `
         SELECT user_id1, user_id2 
         FROM friendships 
@@ -73,7 +73,7 @@ func (ur *UserRepository) GetUserFriends(ctx context.Context, id uint) ([]models
 	}
 	defer rows.Close()
 
-	// Iterate over rows and build the friendships slice
+	// Iterate rows for each friendships
 	for rows.Next() {
 		var friendship models.Friendship
 		if err := rows.Scan(&friendship.UserID1, &friendship.UserID2); err != nil {
@@ -82,14 +82,12 @@ func (ur *UserRepository) GetUserFriends(ctx context.Context, id uint) ([]models
 		friendships = append(friendships, friendship)
 	}
 
-	// Check for row iteration errors
 	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("error reading friendship rows: %w", err)
 	}
 
 	userFriends := make([]models.UserResponse, 0, len(friendships))
 
-	// Retrieve friend user details based on friendships
 	var friendId uint
 	for _, fsh := range friendships {
 
@@ -122,7 +120,6 @@ func (ur *UserRepository) ToggleFriendStatus(ctx context.Context, id, friendId u
 
 	if friendship != nil {
 		// They're friends => remove
-		// fmt.Println("Deleting friendship...")
 		err := ur.deleteFriendship(ctx, id, friendId)
 
 		if err != nil {
@@ -131,7 +128,6 @@ func (ur *UserRepository) ToggleFriendStatus(ctx context.Context, id, friendId u
 
 	} else {
 		// They are not friends => add
-		// fmt.Println("Creating friendship...")
 		err := ur.createFriendship(ctx, id, friendId)
 
 		if err != nil {
@@ -159,7 +155,7 @@ func (ur *UserRepository) getFriendship(ctx context.Context, userID1, userID2 ui
 
 	if err != nil {
 		// Handle the "not found" case
-		if err.Error() == "no rows in result set" {
+		if err == pgx.ErrNoRows {
 			return nil, nil
 		}
 		return nil, fmt.Errorf("could not fetch friendships: %w", err)
@@ -169,13 +165,12 @@ func (ur *UserRepository) getFriendship(ctx context.Context, userID1, userID2 ui
 }
 
 func (ur *UserRepository) createFriendship(ctx context.Context, userID1, userID2 uint) error {
-	// Define the SQL insert query
+
 	query := `
 		INSERT INTO friendships (user_id1, user_id2)
 		VALUES ($1, $2)
 	`
 
-	// Execute the insert query
 	_, err := ur.db.Exec(ctx, query, userID1, userID2)
 	if err != nil {
 		return fmt.Errorf("could not create friendship: %w", err)
@@ -190,13 +185,12 @@ func (ur *UserRepository) deleteFriendship(ctx context.Context, userID1, userID2
 		DELETE FROM friendships
 		WHERE (user_id1 = $1 AND user_id2 = $2) OR (user_id1 = $2 AND user_id2 = $1)
 	`
-	// Execute the delete query
+
 	result, err := ur.db.Exec(ctx, query, userID1, userID2)
 	if err != nil {
 		return fmt.Errorf("could not delete friendship: %w", err)
 	}
 
-	// Check if any row was affected (deleted)
 	if result.RowsAffected() == 0 {
 		return errors.New("friendship not found")
 	}
